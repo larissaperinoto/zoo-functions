271ab57f0b7233e9e5910cfa86a8eb89cf86991b
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-20 20:05:19 -0300

    [Larissa Perinoto] Project zoo functions [Requisito 3]

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  for (let index = 0; index < data.employees.length; index += 1) {
+    const employer = data.employees[index];
+    if (employer.firstName === employeeName || employer.lastName === employeeName) {
+      return employer;
+    }
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
