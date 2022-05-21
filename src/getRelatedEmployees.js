cde9f30a3c347507879cd1e05a7c0ac28b7ff91a
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-21 20:07:07 -0300

    [Larissa Perinoto] Project zoo functions [Requisito 4]

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  for (const objeto of data.employees) {
+    if (objeto.id === id && id != '4b40a139-d4dc-4f09-822d-ec25e819a5ad') {
+      return true
+    }
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  let responsibility = [];
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  for (const employer of data.employees) {
+    if (employer.managers.includes(managerId)) {
+      responsibility.push(`${employer.firstName} ${employer.lastName}`);
+    }
+  }
+  return responsibility;
 }
+
+console.log(getRelatedEmployees("9e7d4524-363c-416a-8759-8aa7e50c0992"))
 
 module.exports = { isManager, getRelatedEmployees };
