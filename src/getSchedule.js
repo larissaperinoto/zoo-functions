0b73fa27a91112b1441a24485d980b23b9fe37c6
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-23 17:18:27 -0300

    [Larissa Perinoto] Project zoo functions [Requisito 9]

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { hours } = data;
+
+function exhibition(hour) {
+  if (hour === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+  return species
+    .filter((specie) => specie.availability.includes(hour))
+    .reduce((acc, curr) => [...acc, curr.name], []);
+}
+
+function officeHour(hour) {
+  if (hour === 'Monday') {
+    return 'CLOSED';
+  }
+  return `Open from ${hours[hour].open}am until ${hours[hour].close}pm`;
+}
+
+function allAnimalsAvaliability(availability) {
+  const schedule = {};
+  Object.keys(availability).forEach((hour) => {
+    schedule[hour] = {
+      officeHour: officeHour(hour),
+      exhibition: exhibition(hour),
+    };
+  });
+  return schedule;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (species.some((specie) => specie.name === scheduleTarget)) {
+    return species.filter((specie) => specie.name === scheduleTarget)[0].availability;
+  } if (Object.keys(allAnimalsAvaliability(hours)).includes(scheduleTarget)) {
+    return {
+      [scheduleTarget]:
+        allAnimalsAvaliability(hours)[scheduleTarget],
+    };
+  }
+  return allAnimalsAvaliability(hours);
 }
 
 module.exports = getSchedule;
