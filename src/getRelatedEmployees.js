47a0748d97e66ce6de727f99a1dd80392fdc8cf9
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-22 23:54:14 -0300

    [Larissa Perinoto] Project zoo functions [Refatora requisito 4]

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,28 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  for (const objeto of data.employees) {
-    if (objeto.id === id && id != '4b40a139-d4dc-4f09-822d-ec25e819a5ad') {
-      return true
-    }
-  }
-  return false;
+  return employees.some((employer) =>
+    employer.id === id && id !== '4b40a139-d4dc-4f09-822d-ec25e819a5ad');
 }
 
 function getRelatedEmployees(managerId) {
-  let responsibility = [];
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 
-  for (const employer of data.employees) {
-    if (employer.managers.includes(managerId)) {
-      responsibility.push(`${employer.firstName} ${employer.lastName}`);
-    }
-  }
-  return responsibility;
+  return employees
+    .filter((employer) => employer.managers.includes(managerId))
+    .reduce((acc, curr) => acc.concat(`${curr.firstName} ${curr.lastName}`), []);
 }
-
-console.log(getRelatedEmployees("9e7d4524-363c-416a-8759-8aa7e50c0992"))
 
 module.exports = { isManager, getRelatedEmployees };
