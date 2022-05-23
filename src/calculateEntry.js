bc906abea11c98ace0841a7d0893e653dfda9375
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-23 15:24:59 -0300

    [Larissa Perinoto] Project zoo functions [Requisito 8]

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
+const visitors = {
+  adult: 0,
+  child: 0,
+  senior: 0,
+};
+
 function countEntrants(entrants) {
-  // seu código aqui
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      visitors.child += 1;
+    } else if (entrant.age >= 50) {
+      visitors.senior += 1;
+    } else {
+      visitors.adult += 1;
+    }
+  });
+  return visitors;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const visitants = [countEntrants(entrants)];
+  let total = 0;
+  Object.keys(prices).forEach((price) => {
+    Object.keys(visitants[0]).forEach((visitor) => {
+      if (price === visitor) {
+        const value = visitors[visitor] * prices[price];
+        total += value;
+      }
+    });
+  });
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
