57d7c11b705f9002bea3831f72cfe1343fbdc18b
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-23 19:44:19 -0300

    [Larissa Perinoto] Project zoo functions [Requisito 10]

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal = employees.find((employer) => employer.id === id).responsibleFor[0];
+  const animalsResidents = species.filter((specie) => specie.id === animal)[0].residents;
+  const oldestAnimal = animalsResidents.reduce((acc, curr) => {
+    let old = acc;
+    if (old.age < curr.age) old = curr;
+    return old;
+  });
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
