8a7d62b0e667f102956dac56b389274bde90c040
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-22 23:35:04 -0300

    [Larissa Perinoto] Project zoo functions [Requisito 5]

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+function animalUndefined(animal) {
+  return species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return animalUndefined(animal);
+  }
+
+  if (Object.keys(animal).length === 1) {
+    return species.filter((specie) => specie.name === animal.specie)[0].residents.length;
+  }
+
+  if (Object.keys(animal).length === 2) {
+    return species.filter((specie) => specie.name === animal.specie)[0]
+      .residents.filter((resident) => resident.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
